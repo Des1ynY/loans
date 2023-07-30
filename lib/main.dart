fdf79a55b7e017a6261c7630b80aac0af34e0868
Des1ynY <i.smetanin.dev@gmail.com>
2023-07-30 22:02:26 +0500

    chore: android firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart' as concurrency;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:loans/src/features/app/widgets/app.dart';
 
-void main() {
+Future<void> main() async {
   final binding = WidgetsFlutterBinding.ensureInitialized()..deferFirstFrame();
+
+  await Firebase.initializeApp();
 
   Bloc.transformer = concurrency.sequential();
 
