7fe851daf83f6b5efc9d08d210a5065beef44dd3
Des1ynY <i.smetanin.dev@gmail.com>
2023-08-03 09:25:03 +0500

    chore: api module rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:api/api.dart';
 import 'package:bloc/bloc.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart' as concurrency;
+import 'package:firebase_api/firebase_api.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:loans/src/features/app/widgets/app.dart';
